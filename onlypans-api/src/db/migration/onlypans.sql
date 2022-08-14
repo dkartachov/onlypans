25cc90816f4441fa0698d5ee624d8904e3aae66c
Denis Kartachov <kartachovd@gmail.com>
2022-08-14 17:58:04 -0400

    add onlypans db create script

---
+++ onlypans-api/src/db/migration/onlypans.sql
@@ -0,0 +1,290 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 14.2
+-- Dumped by pg_dump version 14.2
+
+-- Started on 2022-08-14 17:56:35
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- TOC entry 226 (class 1255 OID 16543)
+-- Name: update_likes(); Type: FUNCTION; Schema: public; Owner: postgres
+--
+
+CREATE FUNCTION public.update_likes() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+-- update total likes of a post from posts table
+DECLARE
+	_post_id integer;
+	_current_likes integer;
+	_new_likes integer;
+BEGIN
+    SELECT COALESCE(OLD.post_id, NEW.post_id) INTO _post_id;
+    SELECT likes INTO _current_likes FROM posts WHERE post_id = _post_id;
+
+	IF (TG_OP = 'INSERT') THEN
+		SELECT NEW.post_id INTO _post_id;
+		SELECT (_current_likes + 1) INTO _new_likes;
+	ELSIF (TG_OP = 'DELETE') THEN
+		SELECT OLD.post_id INTO _post_id;
+		SELECT (_current_likes - 1) INTO _new_likes;
+	END IF;
+	
+	UPDATE posts
+	SET likes = _new_likes
+	WHERE post_id = _post_id;
+	
+	RETURN NULL;
+END;
+$$;
+
+
+ALTER FUNCTION public.update_likes() OWNER TO postgres;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- TOC entry 214 (class 1259 OID 16524)
+-- Name: likes; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.likes (
+    like_id integer NOT NULL,
+    post_id integer NOT NULL,
+    liked_by integer NOT NULL
+);
+
+
+ALTER TABLE public.likes OWNER TO postgres;
+
+--
+-- TOC entry 213 (class 1259 OID 16523)
+-- Name: likes_like_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.likes_like_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.likes_like_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 3338 (class 0 OID 0)
+-- Dependencies: 213
+-- Name: likes_like_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.likes_like_id_seq OWNED BY public.likes.like_id;
+
+
+--
+-- TOC entry 212 (class 1259 OID 16468)
+-- Name: posts; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.posts (
+    post_id integer NOT NULL,
+    user_id integer NOT NULL,
+    content character varying(300),
+    media_url text,
+    likes integer DEFAULT 0 NOT NULL,
+    posted_date timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+
+ALTER TABLE public.posts OWNER TO postgres;
+
+--
+-- TOC entry 211 (class 1259 OID 16467)
+-- Name: posts_post_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.posts_post_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.posts_post_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 3339 (class 0 OID 0)
+-- Dependencies: 211
+-- Name: posts_post_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.posts_post_id_seq OWNED BY public.posts.post_id;
+
+
+--
+-- TOC entry 210 (class 1259 OID 16457)
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users (
+    user_id integer NOT NULL,
+    username character varying(30) NOT NULL,
+    email character varying(60) NOT NULL,
+    password text NOT NULL
+);
+
+
+ALTER TABLE public.users OWNER TO postgres;
+
+--
+-- TOC entry 209 (class 1259 OID 16456)
+-- Name: users_user_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.users_user_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.users_user_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 3340 (class 0 OID 0)
+-- Dependencies: 209
+-- Name: users_user_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.users_user_id_seq OWNED BY public.users.user_id;
+
+
+--
+-- TOC entry 3179 (class 2604 OID 16527)
+-- Name: likes like_id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.likes ALTER COLUMN like_id SET DEFAULT nextval('public.likes_like_id_seq'::regclass);
+
+
+--
+-- TOC entry 3178 (class 2604 OID 16484)
+-- Name: posts post_id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.posts ALTER COLUMN post_id SET DEFAULT nextval('public.posts_post_id_seq'::regclass);
+
+
+--
+-- TOC entry 3175 (class 2604 OID 16460)
+-- Name: users user_id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN user_id SET DEFAULT nextval('public.users_user_id_seq'::regclass);
+
+
+--
+-- TOC entry 3187 (class 2606 OID 16530)
+-- Name: likes likes_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.likes
+    ADD CONSTRAINT likes_pkey PRIMARY KEY (like_id);
+
+
+--
+-- TOC entry 3185 (class 2606 OID 16486)
+-- Name: posts posts_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.posts
+    ADD CONSTRAINT posts_pkey PRIMARY KEY (post_id);
+
+
+--
+-- TOC entry 3189 (class 2606 OID 16532)
+-- Name: likes unique_liked_id_liked_by; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.likes
+    ADD CONSTRAINT unique_liked_id_liked_by UNIQUE (like_id, liked_by);
+
+
+--
+-- TOC entry 3181 (class 2606 OID 16466)
+-- Name: users unique_username; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT unique_username UNIQUE (username);
+
+
+--
+-- TOC entry 3183 (class 2606 OID 16464)
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (user_id);
+
+
+--
+-- TOC entry 3193 (class 2620 OID 16544)
+-- Name: likes update_likes; Type: TRIGGER; Schema: public; Owner: postgres
+--
+
+CREATE TRIGGER update_likes AFTER INSERT OR DELETE ON public.likes FOR EACH ROW EXECUTE FUNCTION public.update_likes();
+
+
+--
+-- TOC entry 3192 (class 2606 OID 16538)
+-- Name: likes fk_liked_by; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.likes
+    ADD CONSTRAINT fk_liked_by FOREIGN KEY (liked_by) REFERENCES public.users(user_id) NOT VALID;
+
+
+--
+-- TOC entry 3191 (class 2606 OID 16533)
+-- Name: likes fk_post_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.likes
+    ADD CONSTRAINT fk_post_id FOREIGN KEY (post_id) REFERENCES public.posts(post_id) NOT VALID;
+
+
+--
+-- TOC entry 3190 (class 2606 OID 16477)
+-- Name: posts fk_user_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.posts
+    ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES public.users(user_id);
+
+
+-- Completed on 2022-08-14 17:56:35
+
+--
+-- PostgreSQL database dump complete
+--
+
