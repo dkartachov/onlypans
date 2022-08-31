08aef831198fbc07a092f8bf397814f3ab9312dc
Denis Kartachov <kartachovd@gmail.com>
2022-08-30 23:06:53 -0400

    add endpoints to get/create posts on behalf of user

--- onlypans-api/src/routes/users.ts
+++ onlypans-api/src/routes/users.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
+import Post from '../types/api/Post';
 import User from '../types/api/User';
+import Token from '../types/Token';
 import MESSAGE from '../utils/Messages';
-import * as PansDb from '../utils/PansDB';
+import * as PansDB from '../utils/PansDB';
 import STATUS from '../utils/Status';
 
 interface Query {
@@ -26,7 +28,7 @@ router.get('/', async (req, res) => {
   }
 
   try {
-    const users = await PansDb.Query<User[]>({
+    const users = await PansDB.Query<User[]>({
       method: 'any',
       query: `SELECT user_id AS id, username, email FROM users LIMIT $1`,
       params: [query.limit || queryDefaults.limit]
@@ -42,7 +44,7 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   try {
-    const user = await PansDb.Query<User>({
+    const user = await PansDB.Query<User>({
       method: 'oneOrNone',
       query: 'SELECT user_id AS id, username, email FROM users WHERE user_id = $1',
       params: [req.params.id]
@@ -53,6 +55,74 @@ router.get('/:id', async (req, res) => {
     }
 
     res.status(STATUS.OK).json(user);
+  } catch (error) {
+    console.log(error);
+
+    res.status(STATUS.INTERNAL_SERVER_ERROR).json(MESSAGE.GENERIC_SERVER_ERROR);
+  }
+});
+
+router.get('/:id/posts', async (req, res) => {
+  const { id } = req.params;
+  const { userId } = req.body.token as Token;
+
+  if (userId !== parseInt(id)) {
+    return res.status(STATUS.FORBIDDEN).json('Get posts unauthorized: user ids do not match');
+  }
+
+  try {
+    const select = `
+      p.post_id AS id,
+      p.user_id AS "userId",
+      u.username,
+      p.content,
+      p.likes,
+      CASE 
+        WHEN l.like_id IS NULL THEN false 
+        ELSE true 
+      END AS liked,
+      p.comments`;
+      
+    const posts = await PansDB.Query<Post[]>({
+      method: 'any',
+      query: `SELECT ${select} FROM posts p
+        LEFT JOIN likes l
+        ON l.post_id = p.post_id
+        JOIN users u
+        ON u.user_id = p.user_id
+        WHERE u.user_id = $1`,
+      params: [id]
+    });
+
+    res.status(STATUS.OK).json(posts);
+  } catch (error) {
+    console.log(error);
+
+    res.status(STATUS.INTERNAL_SERVER_ERROR).json(MESSAGE.GENERIC_SERVER_ERROR);
+  }
+});
+
+router.post('/:id/posts', async (req, res) => {
+  const { id } = req.params;
+  const { userId } = req.body.token as Token;
+  const { content } = req.body;
+
+  if (userId !== parseInt(id)) {
+    return res.status(STATUS.FORBIDDEN).json('Create post unauthorized: user ids do not match');
+  }
+
+  if (!content) {
+    return res.status(STATUS.BAD_REQUEST).json('Missing content');
+  }
+
+  try {
+    await PansDB.Query({
+      method: 'none',
+      query: 'INSERT INTO posts (user_id, content) VALUES ($1, $2)',
+      params: [userId, content]
+    });
+
+    res.sendStatus(STATUS.CREATED);
   } catch (error) {
     console.log(error);
 
